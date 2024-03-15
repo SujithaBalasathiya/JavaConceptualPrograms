830170f06b82d4d3bd3596ead1ac79d7e99ec6f9
934617 <934617@LTIN352257.cts.com>
2024-03-15 21:26:40 +0530

    Update commit

---
+++ AutomationTraining/Programs/Starpattern.java
@@ -0,0 +1,45 @@
+package Programs;
+
+public class Starpattern {
+
+	public static void main(String[] args) {
+		
+		int n=3;
+		
+		//*
+		//**
+		//***
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		
+		//***
+		//***
+		//***
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		
+		//***
+		//**
+		//*
+		for(int i=1;i<=n;i++) {
+			for(int j=n;j>=i;j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
